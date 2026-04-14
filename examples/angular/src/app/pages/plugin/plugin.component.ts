5c1df57790bf269df9f0d8ea2903d5491f0abc86
yanggmtl <80186321+yanggmtl@users.noreply.github.com>
2026-04-14 13:32:35 -0400

    Fix component import issue

--- examples/angular/src/app/pages/plugin/plugin.component.ts
+++ examples/angular/src/app/pages/plugin/plugin.component.ts
@@ -24,7 +24,7 @@ import type {
   TranslationFunction,
   DefinitionPropertyField,
 } from '@formitiva/angular';
-import { Point2DInputComponent } from './point2d-input.component';
+import { Point2DInputComponent } from '../component/point2d-input.component';
 
 // ── Plugin definition ─────────────────────────────────────────────────────────
 
